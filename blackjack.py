54a7b235507e7fea0e7fe6fcfa9df2b1c6bc8e0d
woodsonbruce <woodson@alum.mit.edu>
2025-02-03 12:17:28 +0000

    chore: tidy comments

--- blackjack.py
+++ blackjack.py
@@ -16,11 +16,7 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s",
     filemode="w",
 )
-
-# Creating an object
 logger = logging.getLogger()
-
-# Setting the threshold of logger to DEBUG
 logger.setLevel(logging.DEBUG)
 
 # game constants
